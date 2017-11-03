d1b6ce65b9881bae3809e5f77ac14efedfe165d9
Graham Steele <graham.steele@markforged.com>
2017-11-03 15:00:41 -0400

    moar tasteful changes

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-echo '-----BEGIN RSA PRIVATE KEY-----' > temp.key
-echo $key | sed 's/.*- //' | sed 's/ -.*$//' | tr ' ' '\n' >> temp.key
-echo '-----END RSA PRIVATE KEY-----' >> temp.key
-chmod 600 temp.key
-
-ssh -i temp.key $user@$host 'cd testle; git pull origin master' < /dev/null
-
-rm temp.key
-echo wedgely greetings!
+ssh -oBatchMode=yes $user@$host 'cd testle; git pull origin master' < /dev/null
