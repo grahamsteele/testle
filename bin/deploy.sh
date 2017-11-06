0d8e84d4660ff2efe6887370d0a2f3fe71a37f9a
Graham Steele <graham.steele@markforged.com>
2017-11-06 12:03:05 -0500

    hello

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ssh-keyscan for.theverywedge.com >> ~/.ssh/known_hosts
-ssh -oBatchMode=yes $user@$host 'cd testle; git pull origin master' < /dev/null
+ssh-keyscan $host >> ~/.ssh/known_hosts
+ssh -oBatchMode=yes $user@$host 'cd testle; git pull origin master'
